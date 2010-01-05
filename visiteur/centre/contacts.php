43465fe509ae25da8c4786b85b65a39489b76105
unknown <rtrepos@gmail.com>
2010-01-05 23:01:10 +0100

    googleMap avec traitement d'erreur de navigateur non compatible

--- visiteur/centre/contacts.php
+++ visiteur/centre/contacts.php
@@ -7,7 +7,7 @@
 		GAEC à 3 voies<br>
 		L'Olivet<br>
 		35530 SERVON-SUR-VILAINE<br>
-		Tèl : 02 99 55 28 69<br>
+		Tél : 02 99 55 28 69<br>
 		E-mail : fermeolivet@free.fr<br>
 		Site internet : http://fermeolivet.free.fr<br><br>
 	<li>Horaires d'ouverture</li>
@@ -21,16 +21,12 @@
 <div id="centerpage_contacts_rightID">
 <ul class="centerpage_contacts_menu1CL">	
 		<li>Plan d'accès</li>	
-		<!-- <small><a href="http://maps.google.com/maps?f=q&amp;q=rue+olivet,+servon-sur-vilaine,+35000,+FR&amp;ie=UTF8&amp;om=1&amp;ll=48.1068185,-1.4654957&amp;z=13" style="color:#696969;text-align:left">Agrandir le plan</a></small>	
-		<div id="googleMap" style="width:500px;height:300px"></div> -->
+		 <?php include('visiteur/centre/contacts/googleMap.php'); ?>
+
+
+
 
  </ul>
 </div>
-
-
-<!-- google map API key : pour uniquement (?): http://www.fermeolivet.net/
-ABQIAAAA0Pdh_8EET-n72xP7OCU9VRQMBzOkXylWza1-3on8o8mwezh4PRTp2Xq8TqJZmdjX43w2K_JJ0BFd6w
--->
-
 
 
