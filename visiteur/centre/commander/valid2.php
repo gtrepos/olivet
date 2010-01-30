2d23ba090d48416688824c856abd68ffd184bdbb
unknown <rtrepos@gmail.com>
2010-01-30 18:35:32 +0100

    commande : fix valid2.php

--- visiteur/centre/commander/valid2.php
+++ visiteur/centre/commander/valid2.php
@@ -5,7 +5,7 @@ $daterecup = utf8_encode(strftime($outputAff, strtotime($ajax_daterecup_commande
 echo "Merci pour votre commande : <br>";
 echo "<ul>";
 echo "<li>";
-echo "  un mail vous a été envoyé à l'adresse : $ajax_client_mail ";
+echo "  un mail vous a été envoyé à l'adresse : $mail ";
 echo "</li>";
 echo "<li>";
 echo "  nous vous attendons à la ferme pour le  $daterecup";
